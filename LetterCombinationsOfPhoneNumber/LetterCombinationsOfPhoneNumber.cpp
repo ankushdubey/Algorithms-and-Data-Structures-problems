0adedecc39d9b71fc9018c6d39e0228357b4e89c
ankushdubey <ankush.kv@gmail.com>
2020-03-15 09:32:49 +0000

    [LocalTest]:[LetterCombinationsOfPhoneNumber]:[c++]

--- LetterCombinationsOfPhoneNumber/LetterCombinationsOfPhoneNumber.cpp
+++ LetterCombinationsOfPhoneNumber/LetterCombinationsOfPhoneNumber.cpp
@@ -8,11 +8,41 @@ string options[] = {"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
 class LetterCombinationsOfPhoneNumber {
 public:
     // Implement your solution by completing the below function
-
-    vector<string> letterCombinations(string digits) {
-    	vector<string> res;
-		res.push_back(options[0]);	
-
+		vector<string> res;
+		char index=digits[0];
+		string fil=options[index-'0'];
+		if(fil.size()>0){
+		for(int i=0;i<fil.size();i++)
+            {
+                res.push_back(string(1,fil[i]));
+            }
+		}
+		else{
+            res.push_back("");
+		}
+		for(int k=1;k<digits.size();k++)
+        {
+            index=digits[k];
+            fil=options[index-'0'];
+            if(fil.size()>0)
+            {
+                int ini=res.size();
+                for(int i=0;i<fil.size();i++)
+                {
+                    for(int j=0;j<ini;j++)
+                    {
+                       // cout<<res[j]<<endl;
+                        res.push_back(res[j]+string(1,fil[i]));
+                    }
+                }
+                res.erase(res.begin(),res.begin()+ini);
+            }
+        }
+        sort(res.begin(),res.end());
+        for(int i=0;i<res.size();i++)
+        {
+            cout<<res[i]<<endl;
+        }
         return res;
     }
 };
