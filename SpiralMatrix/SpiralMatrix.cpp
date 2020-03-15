cfb00b565071a85d9abead6376faefcfb5eb1e07
ankushdubey <ankush.kv@gmail.com>
2020-03-15 19:10:13 +0000

    [LocalTest]:[SpiralMatrix]:[c++]

--- SpiralMatrix/SpiralMatrix.cpp
+++ SpiralMatrix/SpiralMatrix.cpp
@@ -9,7 +9,22 @@ public:
     // Implement your solution by completing the below function	
     vector<int> spiralOrder(vector<vector<int>>& matrix) {
         vector<int> res;
-
+		int i,j,n,m;
+		i=0;j=0;
+		n=matrix.size();
+		m=matrix[0].size();
+		while(i<=n && j<=m){
+			for(int k=i;k<m;k++)
+				res.push_back(matrix[i][k]);
+			for(int k=j+1;j<n;j++)
+				res.push_back(matrix[j][m-1]);
+			for(int k=m-2;k>=j;--k)
+				res.push_back(matrix[n-1][k]);
+			for(int k=n-2;k>=j;--k)
+				res.push_back(matrix[k][i]);
+			
+			i++;j++;m--;n--;
+		}
         return res;
     }
 };
