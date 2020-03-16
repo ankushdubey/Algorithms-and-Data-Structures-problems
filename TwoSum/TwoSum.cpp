c60aca705157a6f3eff5d42044aab225713fed05
ankushdubey <ankush.kv@gmail.com>
2020-03-16 23:21:14 +0000

    [LocalTest]:[TwoSum]:[c++]

--- TwoSum/TwoSum.cpp
+++ TwoSum/TwoSum.cpp
@@ -10,16 +10,38 @@ public:
     vector<int> twoSum(vector<int>& nums, int target) {
         vector<int> v(2,0);
 		map<int,int> mp;
+		map<int,int> mp2;
 		for(int i=0;i<(int)nums.size();i++)
 		{
 			mp[nums[i]]=i;
+			mp[nums[i]]++;
 		}
+		int flag=0,val;
 		for(int i=0;i<(int)nums.size();i++)
 		{
-			if(mp.find(target-nums[i])!=mp.end() && mp[target-nums[i]]!=mp[nums[i]])
+			if(mp.find(target-nums[i])!=mp.end())
 			{
+				if((mp[target-nums[i]]!=mp[nums[i]])){
 				v[0]=i;
 				v[1]=mp[target-nums[i]];
+				}
+				else if(mp2[target-nums[i]]>1){
+					flag=1;
+					val=target-nums[i];
+					break;
+				}
+			}
+		}
+		if(flag==1)
+		{
+			int ctr=0;
+			for(int i=0;i<nums.size();i++)
+			{
+				if(nums[i]==val)
+				{
+					v[ctr]=i;
+					ctr++;
+				}
 			}
 		}
 		sort(v.begin(),v.end());
