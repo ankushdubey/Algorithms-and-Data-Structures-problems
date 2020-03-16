4bcc9d19eca2e2fc4ed38289b868edd043e6ed50
ankushdubey <ankush.kv@gmail.com>
2020-03-16 22:02:54 +0000

    [LocalTest]:[StringToIntegerAtoi]:[c++]

--- StringToIntegerAtoi/StringToIntegerAtoi.cpp
+++ StringToIntegerAtoi/StringToIntegerAtoi.cpp
@@ -7,7 +7,24 @@ public:
     // Implement your solution by completing the below function	
     int myAtoi(string str) {
     	int ans = 0;
-
+		if(str.size()==0)
+		return ans;
+		string fin;
+		for(int i=0;i<str.size();i++)
+		{
+			if(str[i]!=' ' && (int(str[i])>=48 && int(str[i])<=57))	
+			{
+				fin.push_back(str[i]);
+			}
+		}
+		int i=0;
+		while(fin[i]=='0')
+		{
+			i++;
+		}
+		fin=fin.substr(i,(int)fin.size()-i);
+		stringstream answer(fin);
+		answer>>ans;
         return ans;
     }
 };
