8818becabea36239fe070cf0b6297d2cdd7e4d28
ankushdubey <ankush.kv@gmail.com>
2020-03-17 19:24:44 +0000

    [LocalTest]:[TrappingRainWater]:[c++]

--- TrappingRainWater/TrappingRainWater.cpp
+++ TrappingRainWater/TrappingRainWater.cpp
@@ -3,17 +3,54 @@
 #include "../crio/cpp/io/ReadMatrix.hpp"
 using namespace std;
 
-class TrappingRainWater {
+class TrappingRainWater
+{
 public:
-	// Implement your solution by completing the below function`	
-	int trap(vector<int>& height) {
-	    int ans = 0;
+	// Implement your solution by completing the below function`
+	int trap(vector<int> &height)
+	{
+		int n = height.size();
+		long long ans = 0;
+		int mx_arr[n], mn_arr[n];
+		int mx = height[n - 1];
+		int mn = height[0];
+		mx_arr[n - 1] = height[n - 1];
+		for (int i = n - 2; i >= 0; --i)
+		{
+			if (height[i] > mx)
+			{
+				mx = height[i];
+				mx_arr[i] = mx;
+			}
+			else
+			{
+				mx_arr[i] = mx;
+			}
+		}
+		for (int i = 0; i < n; i++)
+		{
+			if (height[i] > mn)
+			{
+				mn = height[i];
+				mn_arr[i] = mn;
+			}
+			else
+			{
+				mn_arr[i] = mn;
+			}
+		}
 
-	    return ans;
+		for (int i = 0; i < n; ++i)
+		{
+			long long val = min(mx_arr[i], mn_arr[i]);
+			ans += (val - height[i]);
+		}
+		return ans;
 	}
 };
 
-int main() {
+int main()
+{
 	FastIO();
 	int n;
 	cin >> n;
